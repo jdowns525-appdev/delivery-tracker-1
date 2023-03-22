3e4719555539d9196960d9c049a05e091d0750b3
jdowns525 <downsjosseph@gmail.com>
2023-03-22 03:50:03 +0000

    commit

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,8 +1,11 @@
 class DeliveriesController < ApplicationController
   def index
+    
     matching_deliveries = Delivery.all
 
     @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
+    @working_on = matching_deliveries.where( :status => "working_on")
+    @done = matching_deliveries.where( :status => "recieved")
 
     render({ :template => "deliveries/index.html.erb" })
   end
@@ -19,17 +22,17 @@ class DeliveriesController < ApplicationController
 
   def create
     the_delivery = Delivery.new
-    the_delivery.status = params.fetch("query_status")
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.delivery_date = params.fetch("query_delivery_date")
-    the_delivery.detail = params.fetch("query_detail")
+    
+    the_delivery.detail = params.fetch("query_details")
     the_delivery.description = params.fetch("query_description")
+    the_delivery.status = "received"
+    the_delivery.user_id = session.fetch(:user_id)
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Delivery created successfully." })
+      redirect_to("/", { :notice => "Delivery created successfully." })
     else
-      redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
 
@@ -37,17 +40,15 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.status = params.fetch("query_status")
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.delivery_date = params.fetch("query_delivery_date")
-    the_delivery.detail = params.fetch("query_detail")
-    the_delivery.description = params.fetch("query_description")
+    the_delivery.status = params.fetch("received")
+    #the_delivery.description = params.fetch("q")
+    the_delivery.user_id = session.fetch(:user_id)
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries/#{the_delivery.id}", { :notice => "Delivery updated successfully."} )
+      redirect_to("/deliveries", { :notice => "Delivery updated successfully."} )
     else
-      redirect_to("/deliveries/#{the_delivery.id}", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
 
@@ -57,6 +58,6 @@ class DeliveriesController < ApplicationController
 
     the_delivery.destroy
 
-    redirect_to("/deliveries", { :notice => "Delivery deleted successfully."} )
+    redirect_to("/", { :notice => "Delivery deleted successfully."} )
   end
 end
